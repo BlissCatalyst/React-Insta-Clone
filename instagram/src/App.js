1038ce0c4049fbe6c55fe1688d530303406580ce
Logan Hufstetler <logan.hufstetler@gmail.com>
2019-02-19 13:55:35 -0700

    initialized state

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -4,11 +4,23 @@ import './App.css';
 import dummyData from './dummy-data';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      username: '',
+      thumbnailUrl: '',
+      imageUrl: '',
+      likes: 0,
+      timestamp: '',
+      comments: []
+    }
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          
+
         </header>
       </div>
     );
