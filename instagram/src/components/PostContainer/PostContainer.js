78583fdfe2c6c0314f7706c56729fe23b67e856f
Logan Hufstetler <logan.hufstetler@gmail.com>
2019-02-19 16:02:33 -0700

    posted everything except for the comments and font awesome like and comment buttons.

--- instagram/src/components/PostContainer/PostContainer.js
+++ instagram/src/components/PostContainer/PostContainer.js
@@ -6,9 +6,27 @@ import CommentSection from '../CommentSection/CommentSection';
 const PostContainer = props => {
     return (
         <div>
-            {props.comments.map(comment => (
-                <CommentSection comment={comment} />
-            ))}
+            <section>
+                <img src={props.post.thumbnailUrl} alt={props.post.username} />
+                <h2>{props.post.username}</h2>
+                <div>
+                    <img src={props.post.imageUrl} alt="DummyData Post" />
+                </div>
+                <div>
+                    <h3>{props.post.likes} likes</h3>
+                </div>
+            </section>
+            <section>
+                {props.post.comments.map((comment, index) => (
+                    <CommentSection 
+                    comment={comment}
+                    key={index}
+                    />
+                ))}
+            </section>
+            <div>
+                <h4>{props.post.timestamp}</h4>
+            </div>
         </div>
     );
 }
